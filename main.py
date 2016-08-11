cdd60206324c90fe7154add94b4c6b35a84cc3b9
Fxwfran <Fran@Francos-MBP.home>
2016-08-11 02:13:34 -0300

    PEP - 8
    Agregué el archivo que vamos a usar como módulo de funciones y arreglé
    el PEP8 del trabajo

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-#coding:utf-8
+# coding:utf-8
 import random
+import funciones
 
-class restaurante():
+
+class Restaurante():
     pass
 
+
 # Creando la funcion para cargar los registros al vector
-def init(plato, nom, clas, tip, tiem, prec ):
+def init(plato, nom, clas, tip, tiem, prec):
     plato.nombre = nom
     plato.clasificacion = clas
     plato.tipo = tip
     plato.tiempo = tiem
     plato.precio = prec
+
 
 # La funcion que nos permite visualizar el vector de registros, de manera ordenada. evitando
 # que al poner print() nos muestre la referencia en la memoria
@@ -21,6 +25,7 @@ def write(plato):
     print '- Tiempo:', plato.tiempo, ' '
     print '- Precio:', plato.precio, ' '
 
+
 # Pedimos el largo del vector y creamos uno de n Nones
 def crear_vector():
     n = int(input('Ingrese la cantidad de platos que desea cargar: '))
@@ -28,28 +33,30 @@ def crear_vector():
 
     return vector
 
+
 # Se pasa a cargar los registros en el vector, tambien pregunta si desea ver el vector
 def cargar(vector):
     n = len(vector)
-
     for x in range(n):
-       print()
-       tipo = input('Ingrese el tipo de plato: ')
-       nombre = input('Ingrese el nombre del plato: ')
-       clasificacion = input('Ingrese la clasificacion: ')
-       tiempo = int(input('Ingrese el tiempo de cocción: '))
-       precio = int(input('Ingrese el precio:  '))
+        print()
+        tipo = input('Ingrese el tipo de plato: ')
+        nombre = input('Ingrese el nombre del plato: ')
+        clasificacion = input('Ingrese la clasificacion: ')
+        tiempo = int(input('Ingrese el tiempo de cocción: '))
+        precio = int(input('Ingrese el precio:  '))
 
-       vector[x] = restaurante()
-       init(vector[x],tipo,nombre,clasificacion, tiempo, precio )
+        vector[x] = Restaurante()
+        init(vector[x], tipo, nombre, clasificacion, tiempo, precio)
     print()
+
 
 # Carga del vector de manera aleatoria
 def cargar_aleatorio():
     n = 3
     v = 3 * [None]
     for i in v:
-        v[i] = random.randint(0,2)
+        v[i] = random.randint(0, 2)
+
 
 def mostrar_menu():
     print("1)Consultar Carta")
@@ -62,21 +69,22 @@ def mostrar_menu():
 
 opcion = 0
 
-while opcion != 7:
-    mostrar_menu()
-    opcion = input("Ingrese La opción deseada: ")
-    if opcion == 1:
-        mostrar_carta()
-    elif opcion == 2:
-        precio_promedio()
-    elif opcion == 3:
-        menor_tiempo_coccion()
-    elif opcion == 4:
-        comida_tipo()
-    elif opcion == 5:
-        buscar_y_sugerir
-    elif opcion == 6:
-        menu_del_dia
-    elif opcion== 7:
-        print("Gracias por consultar nuestro menú")
-        break
+if __name__ == "__main__":
+    while opcion != 7:
+        mostrar_menu()
+        opcion = input("Ingrese La opción deseada: ")
+        if opcion == 1:
+            funciones.mostrar_carta()
+        elif opcion == 2:
+            funciones.precio_promedio()
+        elif opcion == 3:
+            funciones.menor_tiempo_coccion()
+        elif opcion == 4:
+            funciones.comida_tipo()
+        elif opcion == 5:
+            funciones.buscar_y_sugerir()
+        elif opcion == 6:
+            funciones.menu_del_dia()
+        elif opcion == 7:
+            print("Gracias por consultar nuestro menú")
+            break

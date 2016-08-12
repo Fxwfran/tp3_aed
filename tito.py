2ef05adf62308a247261651a701392d7d623f756
gonzalomio <gonzalomio@outlook.com>
2016-08-12 20:30:46 -0300

    Aca esta el programa faltan terminar 3 funciones
    en el modulo de funciones completas

--- tito.py
+++ tito.py
@@ -1,150 +1,45 @@
-import random
-comidas = "milanesas", "asado", "hamburguesas", "tortillas","alfajor", "flan", "helado", "frutillas con crema"\
-    ,"vitel tone", "Tabla de fiambres", "empanadas", "piza"
-tipos = "Entrada", "Principal", "Postre"
-clasificaciones = "Sin TACC", "estandar", "Vegetariano", "Light"
-precios = 100, 50, 80, 60, 90
-tiempos = 10, 50, 60, 20, 30
-
-class Restaurante():
-    pass
-
-
-# Creando la funcion para cargar los registros al vector
-def init(plato,tip, nom, clas, tiem, prec):
-    plato.tipo = tip
-    plato.nombre = nom
-    plato.clasificacion = clas
-    plato.tiempo = tiem
-    plato.precio = prec
-
-
-
-
-
-# Pedimos el largo del vector y creamos uno de n Nones
-def crear_vector():
-    n = int(input('Ingrese la cantidad de platos que desea cargar: '))
-    vector = n * [None]
-
-    return vector
-# Crea el vector
-def crear():
-    vector = 5 * [None]
-    return vector
-
-
-# Se pasa a cargar los registros en el vector
-def cargar(vector):
-    n = len(vector)
-    for x in range(n):
-        print()
-        tipo = input('Ingrese el tipo de plato: ')
-        nombre = input('Ingrese el nombre del plato: ')
-        clasificacion = input('Ingrese la clasificacion: ')
-        tiempo = int(input('Ingrese el tiempo de cocción: '))
-        precio = int(input('Ingrese el precio:  '))
-
-        vector[x] = Restaurante()
-        init(vector[x], tipo, nombre, clasificacion, tiempo, precio)
-
-
-
-
-# Carga del vector de manera aleatoria
-def cargar_aleatorio(vector):
-    n = len(vector)
-    for x in range(n):
-        tipo = random.choice(tipos)
-        nombre = random.choice (comidas)
-        clasificacion = random. choice (clasificaciones)
-        tiempo = random.choice(tiempos)
-        precio = random.choice(precios)
-
-        vector[x] = Restaurante()
-        init(vector[x], tipo, nombre, clasificacion, tiempo, precio)
-
-
-
-# Las 3 Funciones ya listas del menu
-def mostrar_carta(vector):
-    for i in vector:
-        print ('- Tipo:', i.tipo, ' ')
-        print ('- Nombre:', i.nombre, ' ')
-        print ('- Clasificacion:', i.clasificacion, ' ')
-        print ('- Tiempo:', i.tiempo,'Min ')
-        print ('- Precio:$',i.precio, ' ')
-        print("\n")
-
-def precio_promedio(vector):
-    suma = 0
-    for i in vector:
-        suma+= i.precio
-
-    promedio = suma / len(vector)
-
-    return promedio
-
-def menor_tiempo_coccion(vector):
-    valor = 999999
-
-    for i in vector:
-        if i.tiempo < valor:
-            valor = i.tiempo
-            comida = i.nombre
-    print("El plato con menor tiempo de cocción es  tiempo de cocción es",comida, "con tiempo", valor)
-
-
-
-#Menu
-def mostrar_menu():
-    print("1)Consultar carta: ")
-    print("2)Calcular precio promedio: ")
-    print("3)Buscar comida con menor tiempo de cocción: ")
-    print("4)Calcular y mostrar la cantidad de comidas por tipo: ")
-    print("5)Buscar en la carta un plato principal: ")
-    print("6)Mostrar el menu del día completo: ")
-    print("7)Salir")
-
-opcion = 0
+import funciones
 
 if __name__ == "__main__":
     manual = input('¿Desea cargar las comidas de forma manual? Y/N')
     if manual == 'Y' or manual == 'y':
-        vector = crear_vector()
-        cargar(vector)
+        vector = funciones.crear_vector()
+        funciones.cargar(vector)
 
     elif manual == "N" or manual == "n":
-        vector = crear()
-        cargar_aleatorio(vector)
+        vector = funciones.crear()
+        funciones.cargar_aleatorio(vector)
 
+funciones.mostrar_menu()
+
+opcion =int(input("Ingrese la opcion deceada"))
 
 while opcion != 7:
-    mostrar_menu()
+    funciones.mostrar_menu()
     opcion = int(input("Ingrese La opción deseada: "))
 
     if opcion == 1:
-        mostrar_carta(vector)
+        funciones.mostrar_carta(vector)
         input("precione enter para continuar\n")
 
     elif opcion == 2:
-        print("El Precio promedio es $",precio_promedio(vector))
+        print("El Precio promedio es $",funciones.precio_promedio(vector))
         input("precione enter para continuar\n")
 
     elif opcion == 3:
-        menor_tiempo_coccion(vector)
+        funciones.menor_tiempo_coccion(vector)
         input("precione enter para continuar\n")
 
     elif opcion == 4:
-        comida_tipo()
+        funciones.comida_tipo()
         input("precione enter para continuar\n")
 
     elif opcion == 5:
-        buscar_y_sugerir()
+        funciones.buscar_y_sugerir()
         input("precione enter para continuar\n")
 
     elif opcion == 6:
-        menu_del_dia()
+        funciones.menu_del_dia()
         input("precione enter para continuar\n")
 
     elif opcion == 7:
